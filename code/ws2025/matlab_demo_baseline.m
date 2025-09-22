11fe31cbf370a55cda891aace906c6bde47e9ab5
Patrik <patrik.valabek@stuba.sk>
2025-09-22 11:37:59 +0200

    added simulation models

---
+++ code/ws2025/matlab_demo_baseline.m
@@ -0,0 +1,19 @@
+terminate(pyenv);
+pyenv('Version','/Users/patrik/miniconda3/envs/neuromancer/bin/python'); % here chenge the path to your library
+
+py.baseline_inference.init()
+
+sim_length = 500;
+
+y_sim = zeros(1, sim_length+1);
+u_sim = ones(1, sim_length);
+
+y0 = 0;
+py.baseline_inference.get_x(y0)
+y_sim(1) = y0;
+
+for i = 1:sim_length
+    y_sim(i+1) = py.baseline_inference.y_plus(u_sim(i));
+end
+
+plot(y_sim)
